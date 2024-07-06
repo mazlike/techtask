7a69ea1f00d0b498d49861782b74a1b806d800c4
mazlike <daniil.maslike@ya.ru>
2024-07-06 13:11:55 +0300

    New vers

--- techtask/settings.py
+++ techtask/settings.py
@@ -52,8 +52,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
+
 
 ROOT_URLCONF = 'techtask.urls'
 
@@ -118,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
